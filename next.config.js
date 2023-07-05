71cf2fabbcd0f6bddf41272f6aedd00cf28c4e3f
KingYoSun <nocking257838@gmail.com>
2023-07-05 23:01:51 +0900

    first setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    PDS_HOST: process.env.PDS_HOST,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
